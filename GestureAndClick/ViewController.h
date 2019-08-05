edb8331a24ac08a9b0d9895803ea44ea6d3e69d4
liuruyong <liu_ruyong@wandu.cn>
2019-08-06 06:58:14 +0800

    Initial Commit

---
+++ GestureAndClick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GestureAndClick
+//
+//  Created by 刘儒勇 on 2019/8/6.
+//  Copyright © 2019 刘儒勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GestureAndClick/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GestureAndClick
+//
+//  Created by 刘儒勇 on 2019/8/6.
+//  Copyright © 2019 刘儒勇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
